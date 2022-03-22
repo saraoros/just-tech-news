5f345118d845d658c4bc7ba4c33baf226e2df12f
Sara Oros Gochez <saraorosgochez@Saras-MacBook-Air.local>
2022-03-21 17:29:22 -0700

    created post-routes

--- models/index.js
+++ models/index.js
@@ -1,5 +1,14 @@
 const User = require('./User');
 const Post = require('./Post');
 
+// create associations
+User.hasMany(Post, {
+    foreignKey: 'user_id',
+})
+
+Post.belongsTo(User, {
+    // We are defining the realtionship of the Post Model to the User
+    foreignKey: 'user_id',
+})
 
 module.exports = { User, Post };
