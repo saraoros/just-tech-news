6b223d9e93c86c676feb5ab490c32eba9cb4c1a8
Sara Oros Gochez <saraorosgochez@Saras-MacBook-Air.local>
2022-03-23 13:53:03 -0700

    Homepage was created

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
-const { Post, User } = require("../models");
-const sequelize = require("../config/connection");
+const { Post, User } = require("../../models");
+const sequelize = require("../../config/connection");
 
 // get all users
 router.get("/", (req, res) => {
